2a2e24e9698e03dad64e379ab527b20f5f09c88a
misatifelix <35029929+misatifelix@users.noreply.github.com>
2018-09-27 15:44:58 +0300

    my authentication file

---
+++ app/api/v2/resources/auth.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+
+class RegisterResource(Resource):
+    def post(self):
+        pass
+        
+class LoginResource(Resource):
+    def post(self):
+        pass
+
+class LogoutResource(Resource):
+    def post(self):
+        pass
